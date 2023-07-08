8a6b98f2d2495da17993a0706a2c3ec7a9b6a496
Nicole Han <nicolehan04@gmail.com>
2023-07-08 03:55:12 -0400

    fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
 import Layout from "./pages/Layout";
@@ -8,7 +8,7 @@ import ProjectsFull from "./pages/ProjectsFull";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
           <Route path="projects-full" element={<ProjectsFull />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
