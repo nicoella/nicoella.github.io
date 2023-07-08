44651d85c456bf702ad7abe5ad132cf56a73d266
Nicole Han <nicolehan04@gmail.com>
2023-07-07 20:34:08 -0400

    navbar into bars on resize

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -2,6 +2,12 @@ import React, { useState } from "react";
 import { Outlet, Link, useLocation } from "react-router-dom";
 import "./Layout.css";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+library.add(faBars);
+
 const Layout = () => {
   const location = useLocation();
   const [currentPage, setCurrentPage] = useState("");
@@ -12,23 +18,44 @@ const Layout = () => {
       setCurrentPage("projects");
     }
   }, [location]);
+
+  function change() {
+    if (document.getElementById("navbar").classList.contains("visible")) {
+      document.getElementById("navbar").classList.add("open");
+      document.getElementById("navbar").classList.remove("visible");
+    } else {
+      document.getElementById("navbar").classList.add("visible");
+      document.getElementById("navbar").classList.remove("open");
+    }
+  }
+
+  function closeNav() {
+    if (document.getElementById("navbar").classList.contains("visible")) {
+      document.getElementById("navbar").classList.add("open");
+      document.getElementById("navbar").classList.remove("visible");
+    }
+  }
+
   return (
     <div class="layout">
       <nav className={`${currentPage === "projects" ? "wider" : ""}`}>
         <div class="left">
-          <a>
+          <a onClick={closeNav}>
             <Link to="/">Nicole Han</Link>
           </a>
         </div>
         <div class="right">
-          <div class="open" id="nav">
-            <a>
+          <div className="open" id="navbar">
+            <a onClick={closeNav}>
               <Link to="/">Home</Link>
             </a>
-            <a>
+            <a onClick={closeNav}>
               <Link to="/projects">Projects</Link>
             </a>
           </div>
+          <i className="closed" onClick={change}>
+            <FontAwesomeIcon className="bars" icon={["fas", "bars"]} />
+          </i>
         </div>
       </nav>
 
